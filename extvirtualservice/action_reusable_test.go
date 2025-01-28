46dcb2aa84f7856b401cea117e4dbad16879210c
antoine.choimet <antoine@steadybit.com>
2025-01-28 10:06:21 +0100

    fix: add unit test for source label override for faults

--- extvirtualservice/action_reusable_test.go
+++ extvirtualservice/action_reusable_test.go
@@ -319,3 +319,147 @@ func Test_attackLifecycle_with_client_restriction(t *testing.T) {
 	require.Nil(t, vs.Spec.Http[1].Fault)
 	require.Len(t, vs.Spec.Http[1].Match, 0)
 }
+
+func Test_attackLifecycle_with_source_label(t *testing.T) {
+	// General preparation
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	client, clientset := getTestClient(stopCh)
+	extclient.Istio = client
+	extconfig.Config.ClusterName = "development"
+
+	_, err := clientset.
+		NetworkingV1beta1().
+		VirtualServices("default").
+		Create(context.Background(), &v1beta1.VirtualService{
+			TypeMeta: v1.TypeMeta{
+				Kind:       "VirtualService",
+				APIVersion: "v1beta1",
+			},
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "shop",
+				Namespace: "default",
+			},
+			Spec: networkingv1beta1.VirtualService{
+				Http: []*networkingv1beta1.HTTPRoute{
+					{
+						Name: "test-route-1",
+						Match: []*networkingv1beta1.HTTPMatchRequest{
+							{
+								Headers: map[string]*networkingv1beta1.StringMatch{
+									"Content-Type": {
+										MatchType: &networkingv1beta1.StringMatch_Regex{Regex: "text/.*"},
+									},
+								},
+								SourceLabels: map[string]string{
+									"env": "prod",
+								},
+							},
+						},
+					},
+					{
+						Name: "test-route-2",
+					},
+				},
+			},
+		}, v1.CreateOptions{})
+	require.NoError(t, err)
+
+	// Prepare call
+	prepareRequest := extutil.JsonMangle(action_kit_api.PrepareActionRequestBody{
+		ExecutionId: uuid.MustParse("22955847-b455-461d-8f9b-61ef1ef05060"),
+		Target: &action_kit_api.Target{
+			Attributes: map[string][]string{
+				"k8s.namespace":              {"default"},
+				"istio.virtual-service.name": {"shop"},
+			},
+		},
+		Config: map[string]interface{}{
+			"delay":      5000.0,
+			"percentage": 69.0,
+			"sourceLabels": []interface{}{
+				map[string]interface{}{"key": "env", "value": "dev"},
+			},
+			"headers":          []any{},
+			"headersMatchType": "exact",
+		},
+	})
+	state := ActionState{}
+	err = prepareVirtualServiceFault(&state, prepareRequest, toHTTPDelayFault)
+	require.NoError(t, err)
+
+	// Start call
+	err = startVirtualServiceFault(context.TODO(), &state)
+	require.NoError(t, err)
+
+	// Check that the VirtualService has a configured fault
+	vs, err := clientset.
+		NetworkingV1beta1().
+		VirtualServices("default").
+		Get(context.Background(), "shop", v1.GetOptions{})
+	require.NoError(t, err)
+	require.Len(t, vs.Spec.Http, 4)
+	require.Equal(t, "steadybit-injected-fault_22955847-b455-461d-8f9b-61ef1ef05060_0", vs.Spec.Http[0].Name)
+	require.NotNil(t, vs.Spec.Http[0].Fault)
+	require.Equal(t, []*networkingv1beta1.HTTPMatchRequest{
+		{
+			Headers: map[string]*networkingv1beta1.StringMatch{
+				"Content-Type": {
+					MatchType: &networkingv1beta1.StringMatch_Regex{Regex: "text/.*"},
+				},
+			},
+			SourceLabels: map[string]string{
+				"env": "dev",
+			},
+		},
+	}, vs.Spec.Http[0].Match)
+	require.Equal(t, "test-route-1", vs.Spec.Http[1].Name)
+	require.Nil(t, vs.Spec.Http[1].Fault)
+	require.Equal(t, []*networkingv1beta1.HTTPMatchRequest{
+		{
+			Headers: map[string]*networkingv1beta1.StringMatch{
+				"Content-Type": {
+					MatchType: &networkingv1beta1.StringMatch_Regex{Regex: "text/.*"},
+				},
+			},
+			SourceLabels: map[string]string{
+				"env": "prod",
+			},
+		},
+	}, vs.Spec.Http[1].Match)
+	require.Equal(t, "steadybit-injected-fault_22955847-b455-461d-8f9b-61ef1ef05060_1", vs.Spec.Http[2].Name)
+	require.NotNil(t, vs.Spec.Http[2].Fault)
+	require.Len(t, vs.Spec.Http[2].Match, 1)
+	require.Equal(t, "test-route-2", vs.Spec.Http[3].Name)
+	require.Nil(t, vs.Spec.Http[3].Fault)
+	require.Len(t, vs.Spec.Http[3].Match, 0)
+
+	// Stop call
+	err = stopVirtualServiceFault(context.TODO(), &state)
+	require.NoError(t, err)
+
+	// Check that the faults were removed from the VirtualService resource
+	vs, err = clientset.
+		NetworkingV1beta1().
+		VirtualServices("default").
+		Get(context.Background(), "shop", v1.GetOptions{})
+	require.NoError(t, err)
+	require.Len(t, vs.Spec.Http, 2)
+	require.Equal(t, "test-route-1", vs.Spec.Http[0].Name)
+	require.Nil(t, vs.Spec.Http[0].Fault)
+	require.Equal(t, []*networkingv1beta1.HTTPMatchRequest{
+		{
+			Headers: map[string]*networkingv1beta1.StringMatch{
+				"Content-Type": {
+					MatchType: &networkingv1beta1.StringMatch_Regex{Regex: "text/.*"},
+				},
+			},
+			SourceLabels: map[string]string{
+				"env": "prod",
+			},
+		},
+	}, vs.Spec.Http[0].Match)
+	require.Equal(t, "test-route-2", vs.Spec.Http[1].Name)
+	require.Nil(t, vs.Spec.Http[1].Fault)
+	require.Len(t, vs.Spec.Http[1].Match, 0)
+}
